ec65bd6f4947c86a29501fe54b7e114ed3a34e87
Nineeee1223rr <103894855+Nineeee1223rr@users.noreply.github.com>
2024-05-31 05:10:32 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -55,48 +55,6 @@ export const initialSettings = {
 export type Settings = typeof initialSettings
 
 export const textGenModels: Model[] = [{
-    id: '@cf/qwen/qwen1.5-14b-chat-awq',
-    name: 'qwen1.5-14b-chat-awq',
-    provider: 'workers-ai',
-    type: 'chat'
-}, 
-{
-    id: '@cf/openchat/openchat-3.5-0106',
-    name: 'openchat-3.5-0106',
-    provider: 'workers-ai',
-    type: 'chat'
-}, 
-{
-    id: '@cf/google/gemma-7b-it-lora',
-    name: 'gemma-7b-it-lora',
-    provider: 'workers-ai',
-    type: 'chat'
-},
-{
-    id: '@hf/thebloke/openhermes-2.5-mistral-7b-awq',
-    name: 'openhermes-2.5-mistral-7b-awq',
-    provider: 'workers-ai',
-    type: 'chat'
-}, 
-{
-    id: '@hf/thebloke/neural-chat-7b-v3-1-awq',
-    name: 'neural-chat-7b-v3-1-awq',
-    provider: 'workers-ai',
-    type: 'chat'
-}, 
-{
-    id: '@hf/nexusflow/starling-lm-7b-beta',
-    name: 'starling-lm-7b-beta',
-    provider: 'workers-ai',
-    type: 'chat'
-}, 
-{
-    id: '@cf/meta/llama-3-8b-instruct',
-    name: 'llama-3-8b-instruct',
-    provider: 'workers-ai',
-    type: 'chat'
-},
-{
     id: '@cf/meta/llama-2-7b-chat-fp16',
     name: 'llama-2-7b-chat-fp16',
     provider: 'workers-ai',
@@ -106,7 +64,7 @@ export const textGenModels: Model[] = [{
     id: '@cf/meta/llama-2-7b-chat-int8',
     name: 'llama-2-7b-chat-int8',
     provider: 'workers-ai',
-    type": 'chat'
+    type: 'chat'
 },
 {
     id: '@cf/mistral/mistral-7b-instruct-v0.1',
@@ -115,178 +73,184 @@ export const textGenModels: Model[] = [{
     type: 'chat'
 },
 {
-    "id": "@hf/thebloke/deepseek-coder-6.7b-base-awq",
-    "name": "deepseek-coder-6.7b-base-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/thebloke/deepseek-coder-6.7b-base-awq',
+    name: 'deepseek-coder-6.7b-base-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/thebloke/deepseek-coder-6.7b-instruct-awq",
-    "name": "deepseek-coder-6.7b-instruct-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/thebloke/deepseek-coder-6.7b-instruct-awq',
+    name: 'deepseek-coder-6.7b-instruct-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/deepseek-ai/deepseek-math-7b-base",
-    "name": "deepseek-math-7b-base",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/deepseek-ai/deepseek-math-7b-base',
+    name: 'deepseek-math-7b-base',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/deepseek-ai/deepseek-math-7b-instruct",
-    "name": "deepseek-math-7b-instruct",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/deepseek-ai/deepseek-math-7b-instruct',
+    name: 'deepseek-math-7b-instruct',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/thebloke/discolm-german-7b-v1-awq",
-    "name": "discolm-german-7b-v1-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/thebloke/discolm-german-7b-v1-awq',
+    name: 'discolm-german-7b-v1-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/tiiuae/falcon-7b-instruct",
-    "name": "falcon-7b-instruct",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/tiiuae/falcon-7b-instruct',
+    name: 'falcon-7b-instruct',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/google/gemma-2b-it-lora",
-    "name": "gemma-2b-it-lora",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/google/gemma-2b-it-lora',
+    name: 'gemma-2b-it-lora',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/google/gemma-7b-it",
-    "name": "gemma-7b-it",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/google/gemma-7b-it',
+    name: 'gemma-7b-it',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/google/gemma-7b-it-lora",
-    "name": "gemma-7b-it-lora",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/google/gemma-7b-it-lora',
+    name: 'gemma-7b-it-lora',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/nousresearch/hermes-2-pro-mistral-7b",
-    "name": "hermes-2-pro-mistral-7b",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/nousresearch/hermes-2-pro-mistral-7b',
+    name: 'hermes-2-pro-mistral-7b',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/thebloke/llama-2-13b-chat-awq",
-    "name": "llama-2-13b-chat-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/thebloke/llama-2-13b-chat-awq',
+    name: 'llama-2-13b-chat-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/meta-llama/llama-2-7b-chat-hf-lora",
-    "name": "llama-2-7b-chat-hf-lora",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/meta-llama/llama-2-7b-chat-hf-lora',
+    name: 'llama-2-7b-chat-hf-lora',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/meta/llama-3-8b-instruct",
-    "name": "llama-3-8b-instruct",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/meta/llama-3-8b-instruct',
+    name: 'llama-3-8b-instruct',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/thebloke/llamaguard-7b-awq",
-    "name": "llamaguard-7b-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/thebloke/llamaguard-7b-awq',
+    name: 'llamaguard-7b-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/meta-llama/meta-llama-3-8b-instruct",
-    "name": "meta-llama-3-8b-instruct",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/meta-llama/meta-llama-3-8b-instruct',
+    name: 'meta-llama-3-8b-instruct',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/thebloke/mistral-7b-instruct-v0.1-awq",
-    "name": "mistral-7b-instruct-v0.1-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/thebloke/mistral-7b-instruct-v0.1-awq',
+    name: 'mistral-7b-instruct-v0.1-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/mistral/mistral-7b-instruct-v0.2",
-    "name": "mistral-7b-instruct-v0.2",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/mistral/mistral-7b-instruct-v0.2',
+    name: 'mistral-7b-instruct-v0.2',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/mistral/mistral-7b-instruct-v0.2-lora",
-    "name": "mistral-7b-instruct-v0.2-lora",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/mistral/mistral-7b-instruct-v0.2-lora',
+    name: 'mistral-7b-instruct-v0.2-lora',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/thebloke/neural-chat-7b-v3-1-awq",
-    "name": "neural-chat-7b-v3-1-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/thebloke/neural-chat-7b-v3-1-awq',
+    name: 'neural-chat-7b-v3-1-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/microsoft/phi-2",
-    "name": "phi-2",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/microsoft/phi-2',
+    name: 'phi-2',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/qwen/qwen1.5-0.5b-chat",
-    "name": "qwen1.5-0.5b-chat",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/qwen/qwen1.5-0.5b-chat',
+    name: 'qwen1.5-0.5b-chat',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/qwen/qwen1.5-1.8b-chat",
-    "name": "qwen1.5-1.8b-chat",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/qwen/qwen1.5-1.8b-chat',
+    name: 'qwen1.5-1.8b-chat',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/qwen/qwen1.5-14b-chat-awq",
-    "name": "qwen1.5-14b-chat-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/qwen/qwen1.5-14b-chat-awq',
+    name: 'qwen1.5-14b-chat-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/qwen/qwen1.5-7b-chat-awq",
-    "name": "qwen1.5-7b-chat-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/qwen/qwen1.5-7b-chat-awq',
+    name: 'qwen1.5-7b-chat-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/defog/sqlcoder-7b-2",
-    "name": "sqlcoder-7b-2",
-    "provider": "workers-ai",
-    "type": "chat
+    id: '@cf/defog/sqlcoder-7b-2',
+    name: 'sqlcoder-7b-2',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/nexusflow/starling-lm-7b-beta",
-    "name": "starling-lm-7b-beta",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@hf/nexusflow/starling-lm-7b-beta',
+    name: 'starling-lm-7b-beta',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/tinyllama/tinyllama-1.1b-chat-v1.0",
-    "name": "tinyllama-1.1b-chat-v1.0",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/tinyllama/tinyllama-1.1b-chat-v1.0',
+    name: 'tinyllama-1.1b-chat-v1.0',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@cf/fblgit/una-cybertron-7b-v2-bf16",
-    "name": "una-cybertron-7b-v2-bf16",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/tinyllama/tinyllama-1.1b-chat-v1.0',
+    name: 'tinyllama-1.1b-chat-v1.0',
+    provider: 'workers-ai',
+    type: 'chat'
 },
 {
-    "id": "@hf/thebloke/zephyr-7b-beta-awq",
-    "name": "zephyr-7b-beta-awq",
-    "provider": "workers-ai",
-    "type": "chat"
+    id: '@cf/fblgit/una-cybertron-7b-v2-bf16',
+    name: 'una-cybertron-7b-v2-bf16',
+    provider: 'workers-ai',
+    type: 'chat'
+},
+{
+    id: '@hf/thebloke/zephyr-7b-beta-awq',
+    name: 'zephyr-7b-beta-awq',
+    provider: 'workers-ai',
+    type: 'chat'
 }]
 
 export const imageGenModels: Model[] = [{
