db44259167f584536332be297dccc20b9210abfc
Nineeee1223rr <103894855+Nineeee1223rr@users.noreply.github.com>
2024-05-31 02:43:51 +0800

    Initial commit

---
+++ i18n.config.ts
@@ -0,0 +1,25 @@
+export default defineI18nConfig(() => ({
+    legacy: false,
+    messages: {
+               en: {
+            setting: 'Setting',
+            use_own_key: 'Use own key',
+            new_chat: 'New chat',
+            // need_image: 'Need image',
+            // waited: 'Waited',
+            // image_max_5MB: 'Image size cannot exceed 5MB',
+            input_password: 'Please enter access password',
+            confirm: 'Confirm',
+            with_history: 'Send with history',
+            without_history: 'Send without history',
+            please_input_text: 'Please input text',
+            // add_image: 'Add image',
+            // support_paste: 'Support paste',
+            send: 'Send',
+            img_gen_steps: 'Image generation steps',
+            text_generation: 'Text generation',
+            image_generation: 'Image generation',
+            system_prompt: 'System prompt',
+        }
+    }
+}))
