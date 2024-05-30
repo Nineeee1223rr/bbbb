a5af2b8ef8f9cafaf23955ffe9d1dec72ec950b7
Nineeee1223rr <103894855+Nineeee1223rr@users.noreply.github.com>
2024-05-31 03:37:14 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -47,9 +47,9 @@ export class Database extends Dexie {
 export const DB = new Database();
 
 export const initialSettings = {
-    openaiKey: '',
+     '',
     image_steps: 20,
-    system_prompt: 'You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.',
+    system_prompt: '   ',
 }
 
 export type Settings = typeof initialSettings
