dcc92a4882716a21ce83b9b004dee440bdf9e0bc
Nineeee1223rr <103894855+Nineeee1223rr@users.noreply.github.com>
2024-05-31 04:27:20 +0800

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -55,6 +55,48 @@ export const initialSettings = {
 export type Settings = typeof initialSettings
 
 export const textGenModels: Model[] = [{
+    id: '@cf/qwen/qwen1.5-14b-chat-awq',
+    name: 'qwen1.5-14b-chat-awq',
+    provider: 'workers-ai',
+    type: 'chat'
+}, 
+{
+    id: '@cf/openchat/openchat-3.5-0106',
+    name: 'openchat-3.5-0106',
+    provider: 'workers-ai',
+    type: 'chat'
+}, 
+{
+    id: '@cf/google/gemma-7b-it-lora',
+    name: 'gemma-7b-it-lora',
+    provider: 'workers-ai',
+    type: 'chat'
+},
+{
+    id: '@hf/thebloke/openhermes-2.5-mistral-7b-awq',
+    name: 'openhermes-2.5-mistral-7b-awq',
+    provider: 'workers-ai',
+    type: 'chat'
+}, 
+{
+    id: '@hf/thebloke/neural-chat-7b-v3-1-awq',
+    name: 'neural-chat-7b-v3-1-awq',
+    provider: 'workers-ai',
+    type: 'chat'
+}, 
+{
+    id: '@hf/nexusflow/starling-lm-7b-beta',
+    name: 'starling-lm-7b-beta',
+    provider: 'workers-ai',
+    type: 'chat'
+}, 
+{
+    id: '@cf/meta/llama-3-8b-instruct',
+    name: 'llama-3-8b-instruct',
+    provider: 'workers-ai',
+    type: 'chat'
+},
+{
     "id": "@cf/meta/llama-2-7b-chat-fp16",
     "name": "llama-2-7b-chat-fp16",
     "provider": "workers-ai",
